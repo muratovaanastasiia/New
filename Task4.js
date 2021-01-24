8ed5b218d03d02fbca43b28e8ec0466234d3ab10
muratovaanastasiia <77929933+muratovaanastasiia@users.noreply.github.com>
2021-01-24 21:11:15 +0300

    Commit Task4

---
+++ Task4.js
@@ -0,0 +1,7 @@
+var a = "# "
+var b = " "
+console.log (b.repeat(4),a)
+console.log (b.repeat(3),a.repeat(2))
+console.log (b.repeat(2),a.repeat(3))
+console.log (b,a.repeat(4))
+console.log ("",a.repeat(5))
